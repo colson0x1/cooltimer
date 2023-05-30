b84444f5ec6fc5b5437594c8121565df5096d798
Colson <colsoncalifornication@gmail.com>
2023-05-30 18:35:04 +0545

    created svg, implimented countdown logic and and styled the UI

--- app.js
+++ app.js
@@ -1,67 +1,24 @@
-console.log(this);
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callbacks) {
-        this.onStart = callbacks.onStart;
-        this.onTick = callbacks.onTick;
-        this.onComplete = callbacks.onComplete;
-    }
-
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-
-  start = () => {
-    if (this.onStart) {
-        this.onStart();
-    }
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  }
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-        this.pause();
-        if (this.onComplete) {
-            this.onComplete();
-        }
-    } else {
-        this.timeRemaining = this.timeRemaining - 1;
-        if (this.onTick) {
-            this.onTick();
-        }
-    }
-  }
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+let duration;
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log('Ayo! Time started letz go');
-    },
-    onTick() {
-        console.log('Woah! Time frkn ticked down');
-    },
-    onComplete() {
-        console.log('BAAM! Finally, its completed!')
-    }
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log('BAAM! Finally, its completed!');
+  },
 });
